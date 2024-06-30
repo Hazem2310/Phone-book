46d2d73e90ca42a6bf7fdd67c4473dfa58f08b99
Hazem2310 <Hazemhb123@gmail.com>
2024-06-30 08:13:10 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+const addButton = document.querySelector('.add-button');
+const removeButtons = document.querySelectorAll('.remove-button');
+const editButtons = document.querySelectorAll('.edit-button');
+
+addButton.addEventListener('click', () => {
+
+});
+
+removeButtons.forEach(button => {
+  button.addEventListener('click', () => {
+
+  });
+});
+
+editButtons.forEach(button => {
+  button.addEventListener('click', () => {
+
+  });
+});
